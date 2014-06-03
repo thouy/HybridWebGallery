f6b9344332171f6ec0e6271dbf461ea74ea367ca
thouy <thouuy@gmail.com>
2014-06-03 17:45:59 +0900

    FIXED : Modularize javascript code
    - Modularize source code
    - Max width/height value be loaded more flexible (not hardcode)

--- js/hybrid.Web.Gallery.js
+++ js/hybrid.Web.Gallery.js
@@ -1,43 +1,71 @@
 /**
- * 1.0
+ * Hybrid Web Gallery 1.0.0
  */
+
 
 
 var currentIndex = 0;
 var screenWidth = 600;
 imgUrlArr = new Array();
 
-function resize(img) {
-	var width = img.width;
-	var height = img.height;
-	//alert(width +"/" + height);
-	var maxWidth = 300;
-	var maxHeight = 300;
-	var resizeWidth;
-	var resizeHeight;
-	if(width > maxWidth || height > maxHeight){ // 원본이미지가 크면 
-		if(width > height){
-			resizeWidth = maxWidth;
-			resizeHeight = Math.round((height * resizeWidth) / width);
-		} else {
-			resizeHeight = maxHeight;
-			resizeWidth = Math.round((width * resizeHeight) / height);
+
+/**
+ *
+ * Utility Object
+ */
+var Utility = (function(){
+	
+	// Closure
+	return {
+		resize : function(imgElem) {
+			var width = imgElem.width;
+			var height = imgElem.height;
+			var maxWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+			var maxHeight = (window.innerHeight > 0) ? window.innerHeight : screen.height;
+			var resizeWidth;
+			var resizeHeight;
+			if(width > maxWidth || height > maxHeight){ // 원본이미지가 크면 
+				if(width > height){
+					resizeWidth = maxWidth;
+					resizeHeight = Math.round((height * resizeWidth) / width);
+				} else {
+					resizeHeight = maxHeight;
+					resizeWidth = Math.round((width * resizeHeight) / height);
+				}
+			}else{ // 원본이미지가 작거나 같으면
+				resizeWidth = width;
+				resizeHeight = height;
+			}
+			imgElem.width = resizeWidth;
+			imgElem.height = resizeHeight;
+		},
+	
+		isMobile : function() {
+			var result = null;
+			var agent = navigator.userAgent;
+			if(agent.match(/(iPhone|iPod|iPad|Android|WebOS|BlackBerry)/))
+				result = true;
+			else if (agent.match(/(MSIE|Chrome|Safari|Firefox)/))
+				result = false;
+			return result;
 		}
-	}else{ // 원본이미지가 작거나 같으면
-		//alert(width +"/" + height);
-		resizeWidth = width;
-		resizeHeight = height;
-	}
-	img.width = resizeWidth;
-	img.height = resizeHeight;
+	};
+})(); // IIFE Pattern
+
+
+
+
+function animate(element, delta, duration) {
+	
 }
 
-Object.extend.fadeIn = function() {
-	this.opacity -= 0.1;
-};
 
-var MobileGallery = {
-		
+
+/**
+ * 
+ * HybridWebGallery Object
+ */
+var HybridWebGallery = {
 	
 	appendDivElement : function(parent, idStr, styleOptions) {
 		var childElement = document.createElement('div');
@@ -67,7 +95,7 @@ var MobileGallery = {
 		var resultImgElem = document.createElement('img');
 		resultImgElem.src = imgUrlArr[currentIndex];
 		
-		resize(resultImgElem);
+		Utility.resize(resultImgElem);
 		galleryScreen.appendChild(resultImgElem);
 		resultImgElem.fadeIn();
 	},
@@ -85,61 +113,63 @@ var MobileGallery = {
 		var resultImgElem = document.createElement('img');
 		resultImgElem.src = imgUrlArr[currentIndex];
 		
-		resize(resultImgElem);
+		Utility.resize(resultImgElem);
 		galleryScreen.appendChild(resultImgElem);
 	},
 	
 	initGallery : function(imgsContainer) {
 		
+		var isMobile = Utility.isMobile();
+		
 		if(imgsContainer.hasChildNodes()) {
 			
-			var imgElements = imgsContainer.getElementsByTagName('img');
-			
-			this.appendDivElement(document.body, 'mg_bg', 'top:0; left:0; z-index:99999999; display:block; position:fixed; opacity:1;');
-			var galleryBg = document.getElementById('mg_bg');
-			
-			this.appendDivElement(galleryBg, 'mg_screen', '');
-			
-			var mgControllerWrapper = document.createElement('div');
-			mgControllerWrapper.setAttribute('id', 'mg_controller_wrapper');
-			
-			var prev = document.createElement('div');
-			prev.setAttribute('id', 'mg_prev_image');
-			prev.innerHTML = 'prev';
-			var next = document.createElement('div');
-			next.setAttribute('id', 'mg_next_image');
-			next.innerHTML = 'next';
-			
-			mgControllerWrapper.appendChild(prev);
-			mgControllerWrapper.appendChild(next);
-			
-			galleryBg.appendChild(mgControllerWrapper);
-			
-			for(var i=0; i<imgElements.length; i++) {
-				imgUrlArr[i] = imgElements[i].src;
-				console.log(i +' = ' + imgUrlArr[i]);
+			if(isMobile) {
+				var imgElements = imgsContainer.getElementsByTagName('img');
+				
+				this.appendDivElement(document.body, 'mg_bg', 'top:0; left:0; z-index:99999999; display:block; position:fixed; opacity:1;');
+				var galleryBg = document.getElementById('mg_bg');
+				
+				this.appendDivElement(galleryBg, 'mg_screen', '');
+				
+				var mgControllerWrapper = document.createElement('div');
+				mgControllerWrapper.setAttribute('id', 'mg_controller_wrapper');
+				
+				var prev = document.createElement('div');
+				prev.setAttribute('id', 'mg_prev_image');
+				prev.innerHTML = 'prev';
+				var next = document.createElement('div');
+				next.setAttribute('id', 'mg_next_image');
+				next.innerHTML = 'next';
+				
+				mgControllerWrapper.appendChild(prev);
+				mgControllerWrapper.appendChild(next);
+				
+				galleryBg.appendChild(mgControllerWrapper);
+				
+				for(var i=0; i<imgElements.length; i++) {
+					imgUrlArr[i] = imgElements[i].src;
+					console.log(i +' = ' + imgUrlArr[i]);
+				}
+				
+				//this.setImage(imgUrlArr[currentIndex]);
+				var galleryScreen = document.getElementById('mg_screen');
+				var resultImgElem = document.createElement('img');
+				resultImgElem.src = imgUrlArr[currentIndex];
+				resultImgElem.onload = function() {
+					Utility.resize(this);
+				};
+				
+				galleryScreen.appendChild(resultImgElem);
+				
+				prev.onclick = this.movePrev;
+				next.onclick = this.moveNext;
+			} else {
+				
 			}
-			
-			//this.setImage(imgUrlArr[currentIndex]);
-			var galleryScreen = document.getElementById('mg_screen');
-			var resultImgElem = document.createElement('img');
-			resultImgElem.src = imgUrlArr[currentIndex];
-			resultImgElem.onload = function() {
-				resize(this);
-			};
-			
-			galleryScreen.appendChild(resultImgElem);
-			
-			
-			prev.onclick = this.movePrev;
-			next.onclick = this.moveNext;
-			
 		} else {
 			throw new Error("The container has no child nodes");
 		}
 	}
-	
-	
 };
 	
 	
